59f7d1d2d48540a844f3149bf858064e54798cdd
Chandan Kumar <chandanofficial.cn@gmail.com>
2025-11-08 23:23:11 +0530

    added another version in the uniq_path

--- DP/62.unique_paths.java
+++ DP/62.unique_paths.java
@@ -21,6 +21,63 @@ class Solution {
         }
         return dp[m-1][n-1];
     }
+
+
+public int uniquePaths2(int m, int n) {
+        // return uniqPathsRecur(m - 1, n - 1);
+        int[][] cache = new int[m][n];
+
+        for (int[] row : cache) {
+            Arrays.fill(row, -1);
+        }
+
+        return uniqPathsMemo(m - 1, n - 1, cache);
+    }
+
+    private int uniqPathsMemo(int m, int n, int[][] cache) {
+        if (m < 0 || n < 0) {
+            return 0;
+        }
+
+        if (m == 0 || n == 0) {
+            return 1;
+        }
+
+        // Bring value from previous back index
+        int backStep;
+        if (cache[m - 1][n] != -1) {
+            backStep = cache[m - 1][n];
+        } else {
+            backStep = uniqPathsMemo(m - 1, n, cache);
+        }
+
+        // Bring value from above path
+        int upStep;
+        if (cache[m][n - 1] != -1) {
+            upStep = cache[m][n - 1];
+        } else {
+            upStep = uniqPathsMemo(m, n - 1, cache);
+        }
+
+        int ans = backStep + upStep;
+        cache[m][n] = ans;
+        return ans;
+    } 
+
+    int uniqPathsRecur(int m, int n) {
+        if (m < 0 || n < 0) {
+            return 0;
+        }
+
+        if (m == 0 && n == 0) {
+            return 1;
+        }
+
+        int backStep = uniqPathsRecur(m - 1, n);
+        int upStep = uniqPathsRecur(m, n - 1);
+
+        return backStep + upStep;
+    }
 }
 
 
