87543a91c3104fe04bd2465b58529bc44368d69e
Chandan Kumar <chandanofficial.cn@gmail.com>
2025-12-08 00:16:37 +0530

    215_kth_largest_element

--- Array/215_kth_largest_element.py
+++ Array/215_kth_largest_element.py
@@ -11,3 +11,16 @@ class Solution:
             k -= 1
             if k == 0:
                 return data
+
+    # Optimized 
+    def findKthLargest2(self, nums: List[int], k: int) -> int:
+        heap = []
+
+        for num in nums:
+            if len(heap) < k:
+                heapq.heappush(heap, num)
+
+            elif heap[0] < num:
+                heapq.heappushpop(heap, num)
+
+        return heap[0]
